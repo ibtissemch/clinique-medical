fa64bd9dcdd5c2a0d8a94850507ebac1e9c1515e
meriamm <meriem919@gmail.com>
2020-01-07 07:28:31 +0100

    RdvPatient

---
+++ Projetcliniquemedical/Controllers/RdvPatientController.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using Projetcliniquemedical.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projetcliniquemedical.Controllers
+{
+    public class RdvPatientController : Controller
+    {
+        // GET: RdvPatient
+        OurDbContext db = new OurDbContext();
+        public ActionResult RendezVousPatient()
+        {
+            IEnumerable<Rdv> rdvs = null;
+
+            using (var client = new HttpClient())
+            {
+                String id = Session["UserID"].ToString();
+                client.BaseAddress = new Uri("https://localhost:44345/");
+                //HTTP GET
+                var responseTask = client.GetAsync("api/Rdvs");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<Rdv>>();
+                    readTask.Wait();
+
+                    rdvs = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    rdvs = Enumerable.Empty<Rdv>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(rdvs);
+        }
+    }
+}
